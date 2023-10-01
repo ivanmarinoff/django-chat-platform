7b58f9f79d6dec5281af529cf5a7781ae5499205
ivanmarinoff <marinoffivan@gmail.com>
2023-10-01 10:30:21 +0300

    Updated .gitignore and requirements.txt

--- chat_platform/settings.py
+++ chat_platform/settings.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
 
 
 
@@ -22,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3&qtj2*tmlthhm7**#^$1n6ra!^)bjs=)0d#nvyh3m0mq*t1(o"
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
